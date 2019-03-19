60efa62c9b936cb4bce0b05626af24445af95ad4
SGeboers <selinageboers@hotmail.com>
2019-03-19 19:39:34 +0100

    deel1

---
+++ FindYoureway/Assets/Scripet/Fire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        
+        if (Input.GetButtonDown("Fire2"))
+        { 
+            GameObject nb = (GameObject)Instantiate(bulletPrefab, this.transform.position, this.transform.rotation);
+            nb.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * 2000);
+        }
+    }
+}
---
+++ FindYoureway/Assets/Scripet/ObjectOnGaol.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class ObjectOnGaol : MonoBehaviour
+{
+    public Text scoretext;
+    public int hoeveelheid;
+    public int winHoeveelheid = 2;
+    public GameObject win;
+    void Update()
+    {
+
+        scoretext.text = "collected " + hoeveelheid +"/" + winHoeveelheid;
+
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Collecteble") hoeveelheid++;
+
+
+        if (hoeveelheid >= winHoeveelheid)
+        {
+
+            win.SetActive(true);
+
+        }
+
+      }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Collecteble") hoeveelheid--;
+    }
+
+
+
+
+}
