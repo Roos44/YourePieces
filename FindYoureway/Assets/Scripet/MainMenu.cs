1fb8bc8f8b4e6365f495e41e547449db5a54c209
SGeboers <selinageboers@hotmail.com>
2019-03-24 13:21:06 +0100

    Update2

--- FindYoureway/Assets/Scripet/MainMenu.cs
+++ FindYoureway/Assets/Scripet/MainMenu.cs
@@ -5,17 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    private void PlayGame()
     {
-
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
-    }
-
-    public void QuitGame ()
-    {
-        Debug.Log("Quit");
-        Application.Quit();
+        SceneManager.LoadScene(1);
     }
 
 
